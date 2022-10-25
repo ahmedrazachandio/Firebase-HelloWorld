ff37bf5438cf9ba04f5683b94f054a1ed67c5a16
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-10-25 12:36:14 +0500

    code push config add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4El0SHGsH31K0luD8DqcpfDfbyHFKk0Q",
+  authDomain: "fir-helloworld-cloud.firebaseapp.com",
+  projectId: "fir-helloworld-cloud",
+  storageBucket: "fir-helloworld-cloud.appspot.com",
+  messagingSenderId: "105671768368",
+  appId: "1:105671768368:web:c640d5649a7f2e50f4b0f5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+
+
+
+
+
+
 
 function App() {
 
   const[postText, setPostText] = useState("");
+  const[posts, setPosts] = useState([]);
   const[isLoading, setIsloading] = useState(false);
 
 
@@ -18,6 +47,7 @@ function App() {
 
   const savePost = (e) => {
     e.preventDefault();
+    console.log("PostText:", postText)
   }
 
 
@@ -28,6 +58,8 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Firebase HelloWorld</h1>
+     <p>Add Todo</p>
 
       <form onSubmit={savePost}>
           
@@ -46,8 +78,7 @@ function App() {
 
 
 
-     <h1>Firebase HelloWorld</h1>
-     <p>Add Todo</p>
+     
      
     </div>
   );
